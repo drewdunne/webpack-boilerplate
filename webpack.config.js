40bc0e1eb822504a0bd3ce65bedfbc92837a6df2
drewdunne <57418947+drewdunne@users.noreply.github.com>
2022-09-19 22:44:06 -0700

    <Improvement> Added Bundle Analyzer
    Also updated gitignore to ignore /dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 module.exports = {
     mode: 'development',
@@ -56,5 +57,6 @@ module.exports = {
             filename: 'index.html',
             template: 'src/template.html',
         }),
+        new BundleAnalyzerPlugin()
     ]
 }
